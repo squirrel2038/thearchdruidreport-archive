79935bad89f94d7cfbff2cba8a9efb7ebfc21534
squirrel2038 <squirrel2038@gmail.com>
2017-06-14 09:04:18 -0500

    make-archive.sh: fix archiving options
    For smaller files, use a 7-zip dictionary larger than the contents to be
    archived.

--- make-archive.sh
+++ make-archive.sh
@@ -5,7 +5,10 @@ set -e
 ./populate_web_cache.py
 ./generate_pages.py
 
-for name in web_cache thearchdruidreport-archive; do
-    rm -f $name.7z
-    7z a -mx=9 $name.tar.xz $name
-done
+rm -fr web_cache.7z
+rm -fr thearchdruidreport-archive.7z
+
+# XXX: Try using a higher -md setting maybe?  You must have enough RAM:
+#    10 * max(<md-setting>, <total-uncompressed-size-of-input-files>)
+7z a -mx=9 -md=200m web_cache.7z                  web_cache
+7z a -mx=9 -md=200m thearchdruidreport-archive.7z thearchdruidreport-archive
