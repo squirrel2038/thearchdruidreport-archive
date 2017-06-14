9719190b5e4baa28845b587665ec2bdaa2cf2565
squirrel2038 <squirrel2038@gmail.com>
2017-06-14 15:16:00 -0500

    Adjust compression options again:
    For the static site archive, -md=200m only helps by 1% and adds a 200MB
    decompression requirement, so remove it.
    
    I'd like to use -md=1000m for the web_cache.7z archive, but it adds a 10GB
    compression requirement, which my current reasonably high-end machine can
    only barely handle.  (If I increase the level to -md=1100m, then 7z exits
    with a cryptic "invalid argument" error.)  Leave a comment explaining the
    situation.

--- make-archive.sh
+++ make-archive.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
+#
+# If you have 10GB of free RAM, use this command-line:
+#    (time TADR_WEB_CACHE_7Z_OPTS=-md=1000m ./make-archive.sh) >& LOG
+#
 set -e
+
+rm -fr web_cache.7z
+rm -fr thearchdruidreport-archive.7z
 
 ./generate_posts_json.py
 ./populate_web_cache.py
 ./generate_pages.py
 
-rm -fr web_cache.7z
-rm -fr thearchdruidreport-archive.7z
+7z a -mx=9 thearchdruidreport-archive.7z thearchdruidreport-archive
+7z a -mx=9 ${TADR_WEB_CACHE_7Z_OPTS} web_cache.7z web_cache
 
-# XXX: Try using a higher -md setting maybe?  You must have enough RAM:
-#    10 * max(<md-setting>, <total-uncompressed-size-of-input-files>)
-7z a -mx=9 -md=200m web_cache.7z                  web_cache
-7z a -mx=9 -md=200m thearchdruidreport-archive.7z thearchdruidreport-archive
+# For a smaller web_archive.7z, set
+#     TADR_WEB_CACHE_7Z_OPTS to -md=1000m
+# You'll need 10GB of RAM to compress, then 1GB to decompress.  The total
+# compression RAM requirement is:
+#     10 * max(<md-setting>, <total-uncompressed-size-of-input-files>)
+# If you don't have enough free RAM, then 7z exits with a cryptic error:
+#     System error:
+#     E_INVALIDARG
