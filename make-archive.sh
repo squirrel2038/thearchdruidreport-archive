ae61254842bca4b803586cfc7f27252031bae543
squirrel2038 <squirrel2038@gmail.com>
2017-06-15 01:57:42 -0500

    Put output archive files in a "dist" subdir

--- make-archive.sh
+++ make-archive.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 set -e
+cd "$(dirname "$0")"
 
-ARCHIVES="thearchdruidreport-archive.tar.xz web_cache.tar.xz"
+rm -fr dist
+mkdir -p dist
 
-for name in $ARCHIVES; do
-    rm -f $name
-    rm -f $name.list
-done
+ARCHIVES=
+ARCHIVES="$ARCHIVES thearchdruidreport-archive.tar.xz"
+ARCHIVES="$ARCHIVES web_cache.tar.xz"
 
 ./generate_posts_json.py
 ./populate_web_cache.py
 ./generate_pages.py
 
-find thearchdruidreport-archive -type f | sort > thearchdruidreport-archive.tar.xz.list
-./list_web_cache_files.py web_cache > web_cache.tar.xz.list
+find thearchdruidreport-archive -type f | sort > dist/thearchdruidreport-archive.tar.xz.list
+./list_web_cache_files.py web_cache            > dist/web_cache.tar.xz.list
 
 for name in $ARCHIVES
     # The tar on macOS doesn't support -a, so specify -J for xz.
-    XZ_OPT='-v9' tar cfJ $name -T $name.list
+    XZ_OPT='-v9' tar cfJ dist/$name -T dist/$name.list
 done
 
-shasum -a256 $ARCHIVES > SHA256SUMS
+(cd dist && shasum -a256 $ARCHIVES) > dist/SHA256SUMS
