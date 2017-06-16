61c2785ff9d7807f9629e9f4c2d84f4baa77ab07
squirrel2038 <squirrel2038@gmail.com>
2017-06-15 20:44:21 -0500

    Add more steps to make-archive.sh
    If ADR_RETRY is set, then run the generation step again after removing
    failed web requests.
    
    Set ADR_RELNAME to add a suffix to the output archives.
    
    Remove the dist/<archive>.list files when finished.

--- make-archive.sh
+++ make-archive.sh
@@ -1,25 +1,40 @@
 #!/bin/sh
+#
+# Command:
+#
+#    (time ADR_RELNAME=CPnnn ADR_RETRY=1 ./make-archive.sh) >& LOG
+#
+
 set -e
 cd "$(dirname "$0")"
 
 rm -fr dist
 mkdir -p dist
 
-SUFFIX=
-if [ "$RELNAME" != "" ]; then
-    SUFFIX=-${RELNAME}
+ADR_SUFFIX=
+if [ "$ADR_RELNAME" != "" ]; then
+    ADR_SUFFIX=-${ADR_RELNAME}
 fi
 
 ARCHIVES=
-ARCHIVES="$ARCHIVES thearchdruidreport-archive$SUFFIX.tar.xz"
-ARCHIVES="$ARCHIVES web_cache$SUFFIX.tar.xz"
+ARCHIVES="$ARCHIVES thearchdruidreport-archive$ADR_SUFFIX.tar.xz"
+ARCHIVES="$ARCHIVES web_cache$ADR_SUFFIX.tar.xz"
 
 ./generate_posts_json.py
+./download_feed_avatars.py list
 ./populate_web_cache.py
 ./generate_pages.py
 
-find thearchdruidreport-archive -type f | sort > dist/thearchdruidreport-archive$SUFFIX.tar.xz.list
-./list_web_cache_files.py web_cache            > dist/web_cache$SUFFIX.tar.xz.list
+if [ "$ADR_RETRY" != "" ]; then
+    # Retry each failed web request a second time.
+    echo "Removing web failed requests and regenerating..."
+    ./remove-web-cache-failures.sh
+    ./populate_web_cache.py
+    ./generate_pages.py
+fi
+
+find thearchdruidreport-archive -type f | sort > dist/thearchdruidreport-archive$ADR_SUFFIX.tar.xz.list
+./list_web_cache_files.py web_cache            > dist/web_cache$ADR_SUFFIX.tar.xz.list
 
 for name in $ARCHIVES; do
     # The tar on macOS doesn't support -a, so specify -J for xz.
